b00411a97d96ad1cf563e5f8904e31054804692b
GaOn <xnoag@icloud.com>
2023-05-08 14:22:59 +0900

    Initial Commit

---
+++ FileManagerTest/FileManagerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerTestApp.swift
+//  FileManagerTest
+//
+//  Created by xnoag on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
